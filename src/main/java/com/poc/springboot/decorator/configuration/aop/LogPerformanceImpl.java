c4eb294512f702e0f50acb9f53ae7cd20c256946
Artur Drummond <arturbdr@gmail.com>
2019-08-18 23:05:40 +0200

    - add: another example of AOP with pointcut

--- src/main/java/com/poc/springboot/decorator/configuration/aop/LogPerformanceImpl.java
+++ src/main/java/com/poc/springboot/decorator/configuration/aop/LogPerformanceImpl.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 
@@ -12,16 +13,29 @@ import org.springframework.util.StopWatch;
 @Aspect
 public class LogPerformanceImpl {
 
+
+    // It will trigger for ServiceExampleA ServiceExampleB and ServiceExampleC
+    @Pointcut("execution(public * execute(..))")
+    public void checkServices() {
+    }
+
+    @Around("checkServices()")
+    public Object checkServices(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        log.info("inside checkServices pointcut");
+        return proceedingJoinPoint.proceed();
+    }
+
     // Check ServiceExampleA to see how to use the annotation
     @Around("@annotation(LogPerformance)")
     public Object logPerformance(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        final StopWatch stopWatch = new StopWatch();
+        final String methodName = proceedingJoinPoint.getSignature().getName();
+        final StopWatch stopWatch = new StopWatch(methodName);
         stopWatch.start();
         try {
             return proceedingJoinPoint.proceed();
         } finally {
             stopWatch.stop();
-            log.info("{} took {} milliseconds to process", proceedingJoinPoint.getSignature(), stopWatch.getTotalTimeMillis());
+            log.info("{} took {} milliseconds to process", methodName, stopWatch.getTotalTimeMillis());
         }
     }
 }
